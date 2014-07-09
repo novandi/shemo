69357dc14ca2229f8bbec2af7d6765436b6ba9a6
novandi <banitama.novandi11@gmail.com>
2014-07-09 17:45:58 +0700

    revisi

---
+++ SHEMO/SHEMO/SHEMO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SHEMO.Startup))]
+namespace SHEMO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
